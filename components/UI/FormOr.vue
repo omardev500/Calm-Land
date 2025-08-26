b7f9fb00a80598725dd050d66496ba52404e88cd
Omar Hossam <omarhossam50@proton.me>
2025-08-26 15:54:26 +0300

    Finished both Login & Register pages in FRONTEND + connected components

---
+++ components/UI/FormOr.vue
@@ -0,0 +1,11 @@
+<template>
+  <span class="relative text-center text-gray-500 before:left-0 before:bg-gray-300 after:left-[57%] after:bg-gray-300 overflow-hidden">or</span>
+</template>
+<style scoped>
+  span::before, span::after {
+    position: absolute;
+    top: 50%; 
+    width: 43%;
+    height: 2px;
+  }
+</style>
---
+++ components/UI/TextLogo.vue
@@ -0,0 +1,8 @@
+<script setup>
+  const props = defineProps({
+    custom: String
+  })
+</script>
+<template>
+  <h1 class="select-none text-center text-blue-600 font-bold" :class="custom">facebook</h1>
+</template>
