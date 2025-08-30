8eb329cdac6c15bacbec965b8d917f6198b1bfb1
Omar Hossam <omarhossam50@proton.me>
2025-08-30 17:36:11 +0300

    Cleaned the project structure

--- composables/useLikeSound.ts
+++
@@ -1,8 +0,0 @@
-export const playSound = (soundPath) => {
-  const audio = new Audio(soundPath);
-  audio.play().catch(error => {
-    console.error('Error playing sound:', error);
-  });
-  return audio;
-};
-
